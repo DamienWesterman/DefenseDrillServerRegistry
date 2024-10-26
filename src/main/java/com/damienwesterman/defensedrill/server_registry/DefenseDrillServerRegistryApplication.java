5979e210b464dc4527ef46b86314f3a819acdddc
Damien Westerman <damien.westerman@snhu.edu>
2024-10-26 09:39:00 -0600

    Just enough to run

--- src/main/java/com/damienwesterman/defensedrill/server_registry/DefenseDrillServerRegistryApplication.java
+++ src/main/java/com/damienwesterman/defensedrill/server_registry/DefenseDrillServerRegistryApplication.java
@@ -3,7 +3,10 @@ package com.damienwesterman.defensedrill.server_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
+@EnableEurekaServer
 public class DefenseDrillServerRegistryApplication {
 
 	public static void main(String[] args) {
